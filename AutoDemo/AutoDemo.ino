562d22a0a71d566611ee754041fd79fdf2b1f9d2
Marc W. Ting <mrting@8si.local>
2016-06-09 16:36:18 -0400

    files

---
+++ AutoDemo/AutoDemo.ino
@@ -0,0 +1,190 @@
+#include <Servo.h>
+
+Servo Servo1L; 
+Servo Servo1R; 
+Servo Servo2L; 
+Servo Servo2R; 
+Servo Servo3L;
+Servo Servo3R; 
+
+int pos=0;
+int rate=0; 
+int x=0;
+
+int photoSensor1= A0;// 2 
+int photoSensor2= A1; //3
+int photoSensor3= A2; //1
+
+int photoValue1=0; 
+int photoValue2=0; 
+int photoValue3=0; 
+
+void setup()
+{
+  Servo1L.attach(11);
+  Servo1R.attach(10);
+  Servo2L.attach(9);
+  Servo2R.attach(6);
+  Servo3L.attach(5);
+  Servo3R.attach(3);
+  Serial.begin(9600);  
+}
+
+void loop()
+{
+  photoValue2= analogRead(photoSensor1);
+  photoValue3= analogRead(photoSensor2);
+  photoValue1= analogRead(photoSensor3);
+  Serial.println(photoValue1);
+  Serial.println(photoValue2);
+  Serial.println(photoValue3);
+  
+  if(photoValue1>photoValue2+50 && photoValue1>photoValue3+50)
+  {
+    tilt1();
+    drop2();
+    drop3();
+    delay(100);
+  }
+  else if(photoValue2>photoValue1+50 && photoValue2>photoValue3+50)
+  {
+    tilt2();
+    drop1();
+    drop3();
+    delay(100);
+  }
+  else if(photoValue3>photoValue2+50 && photoValue3>photoValue1+50)
+  {
+    tilt3();
+    drop1();
+    drop2();
+    delay(100);
+  }
+  else
+  {
+    Servo1L.write(90);
+    Servo1R.write(90);
+    Servo2L.write(90);
+    Servo2R.write(90);
+    Servo3L.write(90);    
+    Servo3R.write(90);        
+  }
+}
+void YmoveUp(int rate)
+{
+  for(pos=90; pos<180; pos+=1)
+  {
+    Servo1L.write(pos);
+    Servo2L.write(pos);
+    Servo3L.write(pos);    
+    Servo1R.write(180-pos);
+    Servo2R.write(180-pos);
+    Servo3R.write(180-pos);    
+    
+    delay(rate);
+  }
+  
+}
+
+void YmoveDown(int rate)
+{
+  for(pos=180; pos>=90; pos-=1)
+  {
+    Servo1L.write(pos);
+    Servo2L.write(pos);
+    Servo3L.write(pos);
+    
+    Servo1R.write(180-pos);
+    Servo2R.write(180-pos);
+    Servo3R.write(180-pos);
+    
+    delay(rate); 
+  }    
+}
+void tilt3()
+{
+  for(pos=90; pos<180; pos++)
+  {
+    Servo3L.write(pos);
+    Servo3R.write(180-pos);
+  }
+}
+void tilt2()
+{
+  for(pos=90; pos<180; pos++)
+  {
+    Servo2L.write(pos);
+    Servo2R.write(180-pos);
+  }
+}
+void tilt1()
+{
+  for(pos=90; pos<180; pos++)
+  {
+    Servo1L.write(pos);
+    Servo1R.write(180-pos);
+  }
+}
+void drop1()
+{
+  for(pos=90; pos>=45; pos-=1)
+  {
+    Servo1L.write(pos);
+    Servo1R.write(180-pos);
+  }
+}
+void drop2()
+{
+  for(pos=90; pos>=45; pos-=1)
+  {
+    Servo2L.write(pos);
+    Servo2R.write(180-pos);
+  }
+}
+void drop3()
+{
+  for(pos=90; pos>=45; pos-=1)
+  {
+    Servo3L.write(pos);
+    Servo3R.write(180-pos);
+  }
+}
+
+//1R and 1L up and down
+  //3R up and 3L down
+  //2R up and 2L down 
+void surge()
+{
+  for(pos=0; pos<=20; pos++)
+  {
+    Servo1L.write(pos);
+    Servo3R.write(pos);
+  }
+  for(pos=20; pos>=0; pos-=1)
+  {
+    Servo1R.write(pos);
+    Servo2L.write(pos);
+  }
+  for(pos=0; pos<=20; pos++)
+  {
+    Servo2R.write(pos);
+    Servo3L.write(pos);
+  }  
+  
+  delay(30);
+  for(pos=20; pos>=0; pos-=1)
+  {
+    Servo1L.write(pos);
+    Servo3R.write(pos);
+  }
+  for(pos=0; pos<=20; pos++)
+  {
+    Servo1R.write(pos);
+    Servo2L.write(pos);
+  }
+  for(pos=20; pos>=0; pos-=1)
+  {
+    Servo2R.write(pos);
+    Servo3L.write(pos);
+  }
+}
---
+++ BackUp/BackUp.ino
@@ -0,0 +1,181 @@
+#include "Wire.h"
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+#include <Servo.h>
+
+Servo Servo1L; 
+Servo Servo1R; 
+Servo Servo2L; 
+Servo Servo2R; 
+Servo Servo3L;
+Servo Servo3R;
+
+int pos=0;
+int rate=0; 
+int x=0;
+
+MPU6050 mpu;
+
+#define OUTPUT_READABLE_YAWPITCHROLL
+#define LED_PIN 13 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
+bool blinkState = false;
+
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64];
+
+Quaternion q;
+VectorFloat gravity; 
+float ypr[3];
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() 
+{
+    mpuInterrupt = true;
+}
+
+
+void setup()
+{
+  Wire.begin();
+  Serial.begin(115200);
+  while (!Serial);
+  
+  Serial.println(F("Initializing I2C devices..."));
+  mpu.initialize();
+  Serial.println(F("Testing device connections..."));
+  Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+  Serial.println(F("\nSend any character to begin DMP programming and demo: "));
+  while (Serial.available() && Serial.read()); // empty buffer
+  while (!Serial.available());                 // wait for data
+  while (Serial.available() && Serial.read()); // empty buffer again
+  Serial.println(F("Initializing DMP..."));
+  devStatus = mpu.dmpInitialize();
+    
+    // make sure it worked (returns 0 if so)
+    if (devStatus == 0) {
+        // turn on the DMP, now that it's ready
+        Serial.println(F("Enabling DMP..."));
+        mpu.setDMPEnabled(true);
+
+        // enable Arduino interrupt detection
+        Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+        attachInterrupt(0, dmpDataReady, RISING);
+        mpuIntStatus = mpu.getIntStatus();
+
+        // set our DMP Ready flag so the main loop() function knows it's okay to use it
+        Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        dmpReady = true;
+
+        // get expected DMP packet size for later comparison
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    } 
+    else 
+    {
+        // ERROR!
+        // 1 = initial memory load failed
+        // 2 = DMP configuration updates failed
+        // (if it's going to break, usually the code will be 1)
+        Serial.print(F("DMP Initialization failed (code "));
+        Serial.print(devStatus);
+        Serial.println(F(")"));
+    }
+
+    // configure LED for output
+    pinMode(LED_PIN, OUTPUT);
+    
+    Servo1L.attach(11);
+    Servo1R.attach(10);
+    Servo2L.attach(9);
+    Servo2R.attach(6);
+    Servo3L.attach(5);
+    Servo3R.attach(3);
+}
+
+void loop()
+{
+  if (!dmpReady) return;
+  while (!mpuInterrupt && fifoCount < packetSize) 
+  {
+        // other program behavior stuff here
+        // .
+        // .
+        // .
+        // if you are really paranoid you can frequently test in between other
+        // stuff to see if mpuInterrupt is true, and if so, "break;" from the
+        // while() loop to immediately process the MPU data
+        // .
+        // .
+        // .
+  }
+  mpuInterrupt = false;
+  mpuIntStatus = mpu.getIntStatus();
+  fifoCount = mpu.getFIFOCount();
+  if ((mpuIntStatus & 0x10) || fifoCount == 1024) 
+  {
+        mpu.resetFIFO();
+        Serial.println(F("FIFO overflow!"));
+  } 
+  else if (mpuIntStatus & 0x02) 
+  {
+        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+        mpu.getFIFOBytes(fifoBuffer, packetSize);
+        fifoCount -= packetSize;
+
+    #ifdef OUTPUT_READABLE_YAWPITCHROLL
+            // display Euler angles in degrees
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetGravity(&gravity, &q);
+            mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+            Serial.print("ypr\t"); //HERE IS WHERE THE THING IS THE THING AND THE VALUES ARE READ. IT IS AN ARRAY.  
+            Serial.print(ypr[0] * 180/M_PI);
+            Serial.print("\t");
+            Serial.print(ypr[1] * 180/M_PI);
+            Serial.print("\t");
+            Serial.println(ypr[2] * 180/M_PI);
+            
+            //autobalancing x axis
+            if (ypr[1]* 180/M_PI > 10 || ypr[1]* 180/M_PI < -10)
+            {
+              Servo1L.write(14*(int(ypr[1]*-180/M_PI))+90);
+              Servo1R.write(180-(14*(int(ypr[1]*-180/M_PI)))+90);
+              Servo2L.write(180-(14*(int(ypr[1]*-180/M_PI)))+90);
+              Servo2R.write(14*(int(ypr[1]*-180/M_PI))+90);
+              Servo3L.write(180-(14*(int(ypr[1]*-180/M_PI)))+90);
+              Servo3R.write(14*(int(ypr[1]*-180/M_PI))+90);
+            }
+              
+            //end autobalancing x axis
+            //autobalancing y axis
+            else if (ypr[2]* 180/M_PI > 10 || ypr[2]* 180/M_PI < -10) 
+            {
+              Servo2L.write(180-(14*(int(ypr[2]*-180/M_PI)))+90);
+              Servo2R.write(14*(int(ypr[2]*-180/M_PI))+90);
+              Servo1L.write(14*(int(ypr[2]*-180/M_PI))+90);
+              Servo1R.write(180-(14*(int(ypr[2]*-180/M_PI)))+90);
+              Servo3L.write(14*(int(ypr[2]*-180/M_PI))+90);
+              Servo3R.write(180-(14*(int(ypr[2]*-180/M_PI)))+90);
+            }
+            //end autobalancing y axis
+            else
+            {
+              Servo2L.write(90);
+              Servo2R.write(90);
+              Servo1L.write(90);
+              Servo1R.write(90);
+              Servo3L.write(90);
+              Servo3R.write(90);
+//              Serial.println("RESETTINGGGGG");
+            }
+        
+        #endif
+         
+        blinkState = !blinkState;
+        digitalWrite(LED_PIN, blinkState);
+  }
+  
+  
+}
